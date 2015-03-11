d10575748310f13fc769647640244fa9474fc265
kurttheviking <kurttheviking@outlook.com>
2015-03-10 20:32:51 -0700

    functional prototype, with tests

---
+++ index.js
@@ -0,0 +1,106 @@
+/* jslint node: true */
+'use strict';
+
+var _ = require('lodash');
+var BPromise = require('bluebird');
+
+
+var Algorithm = function (options) {
+  options = options || {};
+  var self = this;
+
+  if (!(self instanceof Algorithm)) {
+    return new Algorithm(options);
+  }
+
+  var arms = _.isUndefined(options.arms) ? 2 : parseInt(options.arms, 10);
+  var epsilon = _.isUndefined(options.epsilon) ? 0.5 : parseFloat(options.epsilon);
+  var counts = [];
+  var values = [];
+
+  if (arms < 1) {
+    throw new TypeError('invalid arms: cannot be less than 1');
+  }
+  else if (epsilon < 0) {
+    throw new TypeError('invalid epsilon: cannot be less than 0');
+  }
+  else if (epsilon > 1) {
+    throw new TypeError('invalid epsilon: cannot be greater than 1');
+  }
+
+  for (var i=0; i<arms; i++) {
+    counts.push(0);
+    values.push(0);
+  }
+
+  function indexOfMax() {
+    return values.indexOf(Math.max.apply(null, values));
+  }
+
+  var api = {};
+
+  api.n = 0;
+
+  api.draw = function () {
+    return new BPromise(function (resolve) {
+      var arm;
+
+      if (epsilon > _.random(0, 1, true) || api.n === 0) {
+        arm = _.random(0, arms-1);
+      } else {
+        arm = indexOfMax();
+      }
+
+      resolve(arm);
+    });
+  };
+
+  api.load = function (config) {
+    arms = config.arms;
+    epsilon = config.epsilon;
+    counts = config.counts;
+    values = config.values;
+
+    return BPromise.resolve(values);
+  };
+
+  api.reward = function (arm, reward) {
+    return new BPromise(function (resolve, reject) {
+      if (!_.isNumber(arm)) {
+        return reject(new TypeError('missing or invalid required parameter: arm'));
+      }
+      else if (!_.isNumber(reward)) {
+        return reject(new TypeError('missing or invalid required parameter: reward'));
+      }
+      else if (arm >= arms || arm < 0) {
+        return reject(new TypeError('invalid arm: ' + arm + ' not in valid range (0-' + arms.length + ')'));
+      }
+
+      var ct = ++counts[arm];
+      var pre = values[arm];
+      var post = ((ct-1) / ct) * pre + (1/ct) * reward;
+
+      values[arm] = post;
+
+      api.n = _.reduce(counts, function (sum, ct) {
+        return sum + ct;
+      });
+
+      resolve(values);
+    });
+  };
+
+  api.serialize = function () {
+    return BPromise.resolve({
+      arms: arms,
+      epsilon: epsilon,
+      counts: counts.slice(0),
+      values: values.slice(0)
+    });
+  };
+
+  return api;
+};
+
+
+module.exports = Algorithm;
---
+++ test/index.js
@@ -0,0 +1,353 @@
+/* jslint node: true */
+/* global describe, it */
+'use strict';
+
+var _ = require('lodash');
+var BPromise = require('bluebird');
+var chai = require('chai');
+var sinon = require('sinon');
+
+chai.should();
+chai.use(require('sinon-chai'));
+
+var Bandit = require('../index');
+
+
+describe('instance', function () {
+  it ('throws TypeError when passed arms=0', function () {
+    function init () {
+      new Bandit({arms: 0});
+    }
+
+    chai.expect(init).to.throw(TypeError);
+  });
+
+  it ('throws TypeError when passed arms below 0', function () {
+    function init () {
+      new Bandit({arms: -1});
+    }
+
+    chai.expect(init).to.throw(TypeError);
+  });
+
+  it ('throws TypeError when passed below min epsilon', function () {
+    function init () {
+      new Bandit({epsilon: -1});
+    }
+
+    chai.expect(init).to.throw(TypeError);
+  });
+
+  it ('throws TypeError when passed above max epsilon', function () {
+    function init () {
+      new Bandit({epsilon: 2});
+    }
+
+    chai.expect(init).to.throw(TypeError);
+  });
+
+  describe('.draw', function () {
+    it('returns a promise', function () {
+      var bandit = new Bandit();
+
+      var res = bandit.draw();
+
+      var isValid = _.isFunction(res.then);
+      chai.expect(isValid).to.equal(true);
+    });
+
+    it('resolves to a number', function () {
+      var bandit = new Bandit();
+
+      return bandit.draw().then(function (arm) {
+        var isValid = _.isNumber(arm);
+        chai.expect(isValid).to.equal(true);
+      });
+    });
+
+    it('resolves to an arm index less than total arms', function () {
+      var interations = 100;
+      var testArms = [];
+
+      for (var i=0; i<interations; i++) {
+        testArms.push(_.random(1, interations));
+      }
+
+      return BPromise.all(testArms.map(function (arms) {
+        var bandit = new Bandit({arms: arms});
+        return bandit.draw();
+      }))
+      .then(function (results) {
+        var isValid = true;
+
+        testArms.forEach(function (arms, i) {
+          if (results[i] > arms-1) {
+            isValid = false;
+          }
+        });
+
+        chai.expect(isValid).to.equal(true);
+      });
+    });
+
+    it('does not increment observed sample size', function () {
+      var bandit = new Bandit();
+      var expected = bandit.n;
+
+      return bandit.draw().then(function () {
+        var observed = bandit.n;
+        chai.expect(observed).to.equal(expected);
+      });
+    });
+  });
+
+  describe('.reward', function () {
+    it('returns a promise', function () {
+      var bandit = new Bandit();
+
+      var res = bandit.reward(0, 0);
+
+      var isValid = _.isFunction(res.then);
+      chai.expect(isValid).to.equal(true);
+    });
+
+    it('rejects with TypeError if no arm provided', function () {
+      var noop = sinon.spy();
+      var bandit = new Bandit();
+
+      return bandit.reward().then(noop, function (err) {
+        chai.expect(err).to.be.an.instanceof(TypeError);
+        noop.should.have.callCount(0);
+      });
+    });
+
+    it('rejects with TypeError if arm above max', function () {
+      var noop = sinon.spy();
+      var bandit = new Bandit();
+
+      return bandit.reward(3, 0).then(noop, function (err) {
+        chai.expect(err).to.be.an.instanceof(TypeError);
+        noop.should.have.callCount(0);
+      });
+    });
+
+    it('rejects with TypeError if arm below min', function () {
+      var noop = sinon.spy();
+      var bandit = new Bandit();
+
+      return bandit.reward(-1, 0).then(noop, function (err) {
+        chai.expect(err).to.be.an.instanceof(TypeError);
+        noop.should.have.callCount(0);
+      });
+    });
+
+    it('rejects with TypeError if reward is not a number', function () {
+      var noop = sinon.spy();
+      var bandit = new Bandit();
+
+      return bandit.reward(0, '0').then(noop, function (err) {
+        chai.expect(err).to.be.an.instanceof(TypeError);
+        noop.should.have.callCount(0);
+      });
+    });
+
+    it('resolves to updated cumulative rewards', function () {
+      var bandit = new Bandit();
+      var interations = 100;
+
+      var testArm = [];
+      var testReward = [];
+
+      for (var i=0; i<interations; i++) {
+        testArm.push(_.random(0, 1));
+        testReward.push(_.random(0, 1));
+      }
+
+      return BPromise.all(testArm.map(function (arm, i) {
+        var reward = testReward[i];
+        return bandit.reward(arm, reward);
+      }))
+      .then(function (results) {
+        var sum = [0, 0];
+        var count = [0, 0];
+
+        testArm.forEach(function (arm, i) {
+          sum[arm] += testReward[i];
+          count[arm]++;
+        });
+
+        var last = results[results.length-1];
+
+        var isValidArm0 = Math.abs(last[0] - sum[0]/count[0]) < 0.000001;
+        var isValidArm1 = Math.abs(last[1] - sum[1]/count[1]) < 0.000001;
+
+        chai.expect(isValidArm0).to.equal(true);
+        chai.expect(isValidArm1).to.equal(true);
+      });
+    });
+
+    it('increments observed sample size', function () {
+      var bandit = new Bandit();
+      var interations = 100;
+
+      var testArm = [];
+      var testReward = [];
+
+      for (var i=0; i<interations; i++) {
+        testArm.push(_.random(0, 1));
+        testReward.push(_.random(0, 1));
+      }
+
+      return BPromise.all(testArm.map(function (arm, i) {
+        var reward = testReward[i];
+        return bandit.reward(arm, reward);
+      }))
+      .then(function () {
+        chai.expect(bandit.n).to.equal(interations);
+      });
+    });
+  });
+
+  describe('.serialize', function () {
+    it('returns a promise', function () {
+      var bandit = new Bandit();
+
+      var res = bandit.serialize();
+
+      var isValid = _.isFunction(res.then);
+      chai.expect(isValid).to.equal(true);
+    });
+
+    it('resolves to snapshot of algorithm state', function () {
+      var arms = 5;
+      var epsilon = _.random(0, 1, true);
+      var interations = 100;
+
+      var bandit = new Bandit({
+        arms: arms,
+        epsilon: epsilon
+      });
+
+      var testArm = [];
+      var testReward = [];
+
+      for (var i=0; i<interations; i++) {
+        testArm.push(_.random(0, arms-1));
+        testReward.push(_.random(0, 1));
+      }
+
+      return BPromise.all(testArm.map(function (arm, i) {
+        var reward = testReward[i];
+        return bandit.reward(arm, reward);
+      }))
+      .then(function () {
+        return bandit.serialize();
+      })
+      .then(function (pickle) {
+        chai.expect(pickle.arms).to.equal(arms);
+        chai.expect(pickle.epsilon).to.equal(epsilon);
+        chai.expect(pickle.counts.length).to.equal(arms);
+        chai.expect(pickle.values.length).to.equal(arms);
+      });
+    });
+  });
+
+  describe('.load', function () {
+    it('resolves to restored values', function () {
+      var arms = 5;
+      var epsilon = _.random(0, 1, true);
+      var interations = 100;
+
+      var bandit = new Bandit({
+        arms: arms,
+        epsilon: epsilon
+      });
+
+      var expected;
+      var testArm = [];
+      var testReward = [];
+
+      for (var i=0; i<interations; i++) {
+        testArm.push(_.random(0, arms-1));
+        testReward.push(_.random(0, 1));
+      }
+
+      return BPromise.all(testArm.map(function (arm, i) {
+        var reward = testReward[i];
+        return bandit.reward(arm, reward);
+      }))
+      .then(function () {
+        return bandit.serialize();
+      })
+      .then(function (pickle) {
+        expected = pickle.values;
+        return bandit.load(pickle);
+      })
+      .then(function (values) {
+        chai.expect(values).to.deep.equal(expected);
+      });
+    });
+
+    // [KE] previous state is tested by validating that a serialized
+    //      partial update can be restored and built into a state
+    //      equivilent to a known full update
+    it('restores previous state', function () {
+      var arms = 5;
+      var epsilon = _.random(0, 1, true);
+      var interations = 100;
+
+      var bandit = new Bandit({
+        arms: arms,
+        epsilon: epsilon
+      });
+
+      var testArm = [];
+      var testReward = [];
+
+      for (var i=0; i<interations; i++) {
+        testArm.push(_.random(0, arms-1));
+        testReward.push(_.random(0, 1));
+      }
+
+      var testArmPre = testArm.slice(0, interations/2);
+      var testArmPost = testArm.slice(interations/2);
+      var testRewardPre = testReward.slice(0, interations/2);
+      var testRewardPost = testReward.slice(interations/2);
+
+      return BPromise.all(testArmPre.map(function (arm, i) {
+        var reward = testRewardPre[i];
+        return bandit.reward(arm, reward);
+      }))
+      .then(function () {
+        return bandit.serialize();
+      })
+      .then(function (pickle) {
+        return bandit.load(pickle).then(function () {
+          return BPromise.all(testArmPost.map(function (arm, i) {
+            var reward = testRewardPost[i];
+            return bandit.reward(arm, reward);
+          }));
+        });
+      })
+      .then(function (results) {
+        var sum = [0, 0];
+        var count = [0, 0];
+
+        testArm.forEach(function (arm, i) {
+          sum[arm] += testReward[i];
+          count[arm]++;
+        });
+
+        var last = results[results.length-1];
+
+        var isValidArm0 = Math.abs(last[0] - sum[0]/count[0]) < 0.000001;
+        var isValidArm1 = Math.abs(last[1] - sum[1]/count[1]) < 0.000001;
+
+        chai.expect(bandit.n).to.deep.equal(interations);
+        chai.expect(isValidArm0).to.equal(true);
+        chai.expect(isValidArm1).to.equal(true);
+      });
+    });
+  });
+
+});
